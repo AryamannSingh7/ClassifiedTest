a8ae576440f13187cd04d8ab2af155663a6d2b7a
Jeet Desai <jeet.desai@tatvasoft.com>
2022-08-05 18:18:24 +0530

    [#726810] resident dashboard

---
+++ src/packages/blocks/dashboard/src/DashboardStyle.web.tsx
@@ -0,0 +1,127 @@
+export const DashboardStyleWeb: any = {
+  ownerDashboard: {
+    "& .menu": {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+      fontWeight: "600",
+      cursor: "pointer",
+      marginBottom: "18px",
+      background: "#FFFFFF",
+      padding: "5px 20px",
+    },
+    "& .menu .left-icon": {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    "& .menu .right-icon a": {
+      marginLeft: "15px",
+    },
+    "& .dashboard": {
+      overflowY: "auto",
+      overflowX: "hidden",
+      height: "85vh",
+    },
+    "& .dashboard .title": {
+      paddingBottom: "5px",
+    },
+    "& .dashboard .title h5": {
+      fontWeight: "600",
+      fontFamily: "GothamMedium",
+    },
+    "& .right-image": {
+      padding: "25px 60px",
+    },
+    "& .MuiCard-root": {
+      padding: "20px 0",
+      boxShadow: "none",
+      textAlign: "center",
+    },
+    "& a": {
+      textDecoration: "none !important",
+    },
+    "& .MuiCard-root .image": {
+      padding: "20px",
+      border: "3px solid #F8F9FE",
+      display: "inline-block",
+      borderRadius: "50%",
+      marginBottom: "20px",
+    },
+    "& .MuiCard-root h4": {
+      marginBottom: "20px",
+      textTransform: "capitalize",
+    },
+    "& .MuiCard-root .state p": {
+      marginBottom: "5px",
+      textTransform: "capitalize",
+    },
+    "& .MuiCard-root button": {
+      padding: "5px 11px",
+      minWidth: "140px",
+      borderRadius: "25px",
+      fontWeight: "600",
+      color: "#8A8A8A",
+      background: "#F6F6F6",
+    },
+    "& .MuiCard-root button.yellow": {
+      background: "#FEF9F3",
+      color: "#FD9048",
+    },
+    "& .MuiCard-root.big-box": {
+      paddingBottom: "0",
+    },
+    "& .MuiCard-root .content-box": {
+      display: "flex",
+      alignItems: "flex-end",
+      justifyContent: "space-evenly",
+    },
+    "& .MuiCard-root .content-box .left-content": {
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column",
+      marginBottom: "20px",
+      width: "40%",
+      textAlign: "center",
+    },
+    "& .MuiCard-root .content-box .left-content .state": {
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column",
+    },
+    "& .MuiCard-root .content-box .right-content .state": {
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column",
+    },
+    "& .MuiCard-root .content-box .right-content": {
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column",
+      marginBottom: "20px",
+      width: "40%",
+      textAlign: "center",
+    },
+    "& .MuiCard-root .content-box .center-content": {
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column",
+      width: "20%",
+    },
+    "& .MuiCard-root .content-box .center-content .vertical-line": {
+      width: "3px",
+      height: "30px",
+      background: "#F8F9FE",
+    },
+    "& .MuiCard-root .content-box .center-content .image": {
+      marginBottom: "0px",
+    },
+    "& .MuiCard-root .content-box .center-content .image.text": {
+      padding: "15px",
+    },
+    "& .MuiCard-root .content-box .center-content .image.text h4": {
+      marginBottom: "0px",
+      color: "#FD9048",
+    },
+  },
+};
