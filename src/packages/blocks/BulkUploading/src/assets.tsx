6a29e7e0abc74c88bbe62606d49a7c673dbe8c13
Jeet Desai <jeetdesai995@gmail.com>
2022-09-06 19:25:48 +0530

    fix changes in scheduled meeting

--- src/packages/blocks/BulkUploading/src/assets.tsx
+++ src/packages/blocks/BulkUploading/src/assets.tsx
@@ -1,2 +1,8 @@
 export const imgPasswordVisible = require("../assets/ic_password_visible.png");
 export const imgPasswordInVisible = require("../assets/ic_password_invisible.png");
+
+export const SearchIconImage = require("../assets/search.png");
+export const CommentIcon = require("../assets/comment.png");
+export const AcceptIcon = require("../assets/accept.png");
+export const RejectIcon = require("../assets/reject.png");
+export const AwaitIcon = require("../assets/await.png");
