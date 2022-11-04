a8445d293e782784edb682d090e7a1406dba9f07
Jeet Desai <jeet.desai@tatvasoft.com>
2022-11-04 18:18:11 +0530

    dashboard image

--- src/packages/components/src/DashboardCard/index.tsx
+++ src/packages/components/src/DashboardCard/index.tsx
@@ -5,7 +5,7 @@ const DashboardCard = ({ image, heading, title, value }: any) => {
   return (
     <Card>
       <div className="image">
-        <img src={image} alt="keyhand" />
+        <img src={image} alt="" style={{ width: "25px", height: "25px" }} />
       </div>
       <h4 className="heading">{heading}</h4>
       <div className="state">
