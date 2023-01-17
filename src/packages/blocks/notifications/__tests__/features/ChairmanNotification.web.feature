8b57dbe43dc7d9b9f46736279153c2528f9a8b8a
Jeet Desai <jeet.desai@tatvasoft.com>
2023-01-17 15:41:45 +0530

    [910492] resolved bugs

--- src/packages/blocks/notifications/__tests__/features/ChairmanNotification.web.feature
+++ src/packages/blocks/notifications/__tests__/features/ChairmanNotification.web.feature
@@ -4,3 +4,7 @@ Feature: ChairmanNotification
         Given I am a User loading ChairmanNotification
         When I navigate to the ChairmanNotification
         Then ChairmanNotification will load with out errors
+        And Should load task notification
+        And Should load message notification
+        And Should delete the single notification
+        And Should update read un-read status of notification
--- src/packages/blocks/notifications/__tests__/step-definitions/ChairmanNotification.web.steps.tsx
+++ src/packages/blocks/notifications/__tests__/step-definitions/ChairmanNotification.web.steps.tsx
@@ -5,6 +5,11 @@ import { componentProps } from "../../../../components/src/TestCase/ComponentPro
 import ChairmanNotification from "../../src/ChairmanNotification.web";
 import { NotificationStyle } from "../../src/NotificationStyle.web";
 import { BrowserRouter } from "react-router-dom";
+import { runEngine } from "../../../../framework/src/RunEngine";
+import { notificationMockData } from "../../../../components/src/TestCase/NotificationMockData.web";
+import MessageEnum, { getName } from "../../../../framework/src/Messages/MessageEnum";
+import { Message } from "../../../../framework/src/Message";
+import { Tab } from "@material-ui/core";
 
 const ChairmanNotificationProps = componentProps("ChairmanNotification", NotificationStyle);
 
@@ -31,6 +36,45 @@ defineFeature(feature, (test) => {
 
     then("ChairmanNotification will load with out errors", async () => {
       expect(ChairmanNotificationMountWrapper).toMatchSnapshot();
+    });
+
+    then("Should load task notification", async () => {
+      const taskTabSpy = jest.spyOn(ChairmanNotificationMountWrapper.find(Tab).at(0).props(), "onClick");
+      ChairmanNotificationMountWrapper.find(Tab).at(0).props().onClick();
+      expect(taskTabSpy).toHaveBeenCalled();
+
+      let taskNotificationList = new Message(getName(MessageEnum.RestAPIResponceMessage));
+      taskNotificationList.addData(getName(MessageEnum.RestAPIResponceDataMessage), taskNotificationList);
+      taskNotificationList.addData(getName(MessageEnum.RestAPIResponceSuccessMessage), { data: [notificationMockData] });
+      instance.GetChairmanNotificationListCallId = taskNotificationList;
+      runEngine.sendMessage("Notification", taskNotificationList);
+    });
+
+    then("Should load message notification", async () => {
+      const messageTabSpy = jest.spyOn(ChairmanNotificationMountWrapper.find(Tab).at(1).props(), "onClick");
+      ChairmanNotificationMountWrapper.find(Tab).at(1).props().onClick();
+      expect(messageTabSpy).toHaveBeenCalled();
+
+    });
+
+    then("Should delete the single notification", async () => {
+      instance.deleteSingleNotification(1);
+
+      let deleteSingleNotification = new Message(getName(MessageEnum.RestAPIResponceMessage));
+      deleteSingleNotification.addData(getName(MessageEnum.RestAPIResponceDataMessage), deleteSingleNotification);
+      deleteSingleNotification.addData(getName(MessageEnum.RestAPIResponceSuccessMessage), { data: [notificationMockData] });
+      instance.DeleteNotificationCallId = deleteSingleNotification;
+      runEngine.sendMessage("Notification", deleteSingleNotification);
+    });
+
+    then("Should update read un-read status of notification", async () => {
+      instance.updateReadNotificationStatus(1, true);
+
+      let statusNotification = new Message(getName(MessageEnum.RestAPIResponceMessage));
+      statusNotification.addData(getName(MessageEnum.RestAPIResponceDataMessage), statusNotification);
+      statusNotification.addData(getName(MessageEnum.RestAPIResponceSuccessMessage), { data: [notificationMockData] });
+      instance.UpdateReadStatusCallId = statusNotification;
+      runEngine.sendMessage("Notification", statusNotification);
     });
   });
 });
