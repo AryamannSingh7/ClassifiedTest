8454b588148ed118e01a1b1648916b871b5f6891
akshay <akshay>
2023-01-25 10:00:04 +0530

    fix issues

--- src/packages/blocks/user-profile-basic/src/CommunityRequestManagement.web.tsx
+++ src/packages/blocks/user-profile-basic/src/CommunityRequestManagement.web.tsx
@@ -37,19 +37,14 @@ import Grid from '@material-ui/core/Grid';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import { withRouter } from 'react-router';
-import Loader from "../../../components/src/Loader.web";
-import { Input } from "react-native-elements";
-import * as Yup from "yup";
-import CountryCodeSelector from "../../country-code-selector/src/CountryCodeSelector";
 import CommunityUserProfileController, { Props } from "./communityManagementController.web";
 import DashboardHeader from "../../dashboard/src/DashboardHeader.web";
 import ChairmanSidebar from "../../dashboard/src/ChairmanSidebar.web";
 import { withTranslation } from 'react-i18next';
 import '../../../web/src/i18n.js';
-import PeopleOutlinedIcon from '@material-ui/icons/PeopleOutlined';
-import MailOutlineOutlinedIcon from '@material-ui/icons/MailOutlineOutlined';
-import CreateOutlinedIcon from '@material-ui/icons/CreateOutlined';
 import Tooltip from '@material-ui/core/Tooltip';
+import {dashBoard} from "./dash.js";
+
 //resorces
 import { invite, addgroup, newMember, info, cancle, user_icon, email_icon, phone_icon, building, unit } from "./assets";
 
@@ -593,111 +588,7 @@ CANCEL
 //@ts-ignore
 export default withTranslation()(withStyles(dashBoard)(withRouter(CommunityRequestManagement))); 
 
-const dashBoard = {
-  navigation: {
-    display: "flex",
-    justifyContent: "space-between",
-  },
-  subHeading: {
-    fontWeight: 600,
-    // marginTop: 15,
-  },
-  invitationCont:{
-    fontWeight: 600,
-    margin:'10px 0px 10px 0px'
-  },
-  inviteTitle:{
-    margin:'10px 0px 10px 0px'
-  },
-  SideBar: {
-    background: "#f9f6f6",
-    position: "relative",
-    paddingBottom: 150,
-  },
-  gaMemberCard:{
-    display: "grid",
-    gridTemplateColumns: "4fr 4fr 4fr",
-    gap: 20
-  },
-  managementPaper:{
-    padding:20
-  },
-  imgRound:{
-    border: "2px solid #F7F9FE",
-    borderRadius: "100%",
-    height: 50,
-    width: 50
-},
-mailIcon:{
-    padding:8
-},
-invitemember:{
-    border: "2px solid #F7F9FE",
-    borderRadius: "100%",
-    height: 50,
-    width: 50,
-    backgroundColor:"#FC8434"
-},
-inviteIcon:{
-    padding:13
-},
-cancleIcon:{
-    position:"absolute",
-    top:15,
-    right:15
-},
-modalCacle:{
-  top:15,
-  right:15,
-  float:"right",
-  cursor:"pointer"
-},
-invitationReq:{
-    marginTop:30
-},
-facility: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    borderBottom:"1px solid #f8f8f8",
-    cursor:"pointer"
-  },
-  modal:{
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  paper: {
-    backgroundColor: "#fff",
-    borderRadius: '10px',
-    // boxShadow: theme.shadows[5],
-    // padding: "16px 32px 24px",
-    width:"700px"
-},
-  formLabels:{
-    paddingLeft:35
-},
-  labelsStyle:{
-    color:"#212121",
-    margin:"10px 0px 10px 0px"
-},
-formLeftIcn:{
-  position:"absolute",
-  left: 20,
-  top: 44,
-  color: "#b9b9b9"
-},
-inviteInput:{
-  padding: "18px 18px 18px 50px",
-  color: "#b5b5b5",
-  borderRadius: "10px",
-  border: "1px solid #e9dede",
-  backgroundColor: "#f9f9f9",
-  fontSize: "16px",
-  outline: 0,
-  width:"100%"
-}
-};
+
 
 const RequestManagementDetailPopover = withStyles({
     paper: {
---
+++ src/packages/blocks/user-profile-basic/src/dash.js
@@ -0,0 +1,107 @@
+const dashBoard = {
+    navigation: {
+      display: "flex",
+      justifyContent: "space-between",
+    },
+    subHeading: {
+      fontWeight: 600,
+      // marginTop: 15,
+    },
+    invitationCont:{
+      fontWeight: 600,
+      margin:'10px 0px 10px 0px'
+    },
+    inviteTitle:{
+      margin:'10px 0px 10px 0px'
+    },
+    SideBar: {
+      background: "#f9f6f6",
+      position: "relative",
+      paddingBottom: 150,
+    },
+    gaMemberCard:{
+      display: "grid",
+      gridTemplateColumns: "4fr 4fr 4fr",
+      gap: 20
+    },
+    managementPaper:{
+      padding:20
+    },
+    imgRound:{
+      border: "2px solid #F7F9FE",
+      borderRadius: "100%",
+      height: 50,
+      width: 50
+  },
+  mailIcon:{
+      padding:8
+  },
+  invitemember:{
+      border: "2px solid #F7F9FE",
+      borderRadius: "100%",
+      height: 50,
+      width: 50,
+      backgroundColor:"#FC8434"
+  },
+  inviteIcon:{
+      padding:13
+  },
+  cancleIcon:{
+      position:"absolute",
+      top:15,
+      right:15
+  },
+  modalCacle:{
+    top:15,
+    right:15,
+    float:"right",
+    cursor:"pointer"
+  },
+  invitationReq:{
+      marginTop:30
+  },
+  facility: {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      borderBottom:"1px solid #f8f8f8",
+      cursor:"pointer"
+    },
+    modal:{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    paper: {
+      backgroundColor: "#fff",
+      borderRadius: '10px',
+      // boxShadow: theme.shadows[5],
+      // padding: "16px 32px 24px",
+      width:"700px"
+  },
+    formLabels:{
+      paddingLeft:35
+  },
+    labelsStyle:{
+      color:"#212121",
+      margin:"10px 0px 10px 0px"
+  },
+  formLeftIcn:{
+    position:"absolute",
+    left: 20,
+    top: 44,
+    color: "#b9b9b9"
+  },
+  inviteInput:{
+    padding: "18px 18px 18px 50px",
+    color: "#b5b5b5",
+    borderRadius: "10px",
+    border: "1px solid #e9dede",
+    backgroundColor: "#f9f9f9",
+    fontSize: "16px",
+    outline: 0,
+    width:"100%"
+  }
+  };
+
+  export {dashBoard}; 
