f4763f97eadd8fe70c6b65a2507eca74d651b06c
Jeet Desai <jeet.desai@tatvasoft.com>
2023-03-01 19:31:52 +0530

    [#986639] [#986638] [#980481] [#980479] [#980478] [#980477] [#980476] [#980475] [#980474] [#980473] budget details

--- src/packages/blocks/dashboard/src/DashboardBudgetController.web.tsx
+++ src/packages/blocks/dashboard/src/DashboardBudgetController.web.tsx
@@ -8,6 +8,7 @@ import { runEngine } from "../../../framework/src/RunEngine";
 import moment from "moment";
 import { ApiCatchErrorResponse, ApiErrorResponse } from "../../../components/src/APIErrorResponse";
 import { apiCall } from "../../../components/src/APICallComponent/index.web";
+import { toast } from "react-hot-toast";
 // Customizable Area End
 
 export const configJSON = require("./config.js");
@@ -27,6 +28,24 @@ interface S {
   filterYear: any;
   buildingList: any[];
   filterBuilding: string;
+
+  currency: string;
+
+  budgetAmount: string;
+  budgetCollected: string;
+  rentDue: string;
+  rentCollected: string;
+  member: string;
+  totalExpense: string;
+  rateUnsold: string;
+  rateSold: string;
+  totalBudget: string;
+
+  approveAmount: string;
+  facility: any[];
+
+  expenseBreakdown: any[];
+  collectedFee: any[];
   // Customizable Area End
 }
 interface SS {
@@ -37,6 +56,8 @@ export default class DashboardBudgetController extends BlockComponent<Props, S, 
   // Customizable Area Start
   GetBudgetDashboardYearListCallId: any;
   GetAllBuildingListCallId: any;
+  GetBudgetDashboardDataCallId: any;
+  GetBudgetDetailsCallId: any;
   // Customizable Area End
 
   constructor(props: Props) {
@@ -53,6 +74,24 @@ export default class DashboardBudgetController extends BlockComponent<Props, S, 
       filterYear: moment().year(),
       buildingList: [],
       filterBuilding: "",
+
+      currency: "",
+
+      budgetAmount: "",
+      budgetCollected: "",
+      rentDue: "",
+      rentCollected: "",
+      member: "",
+      totalExpense: "",
+      rateSold: "",
+      rateUnsold: "",
+      totalBudget: "",
+
+      approveAmount: "",
+      facility: [],
+
+      expenseBreakdown: [],
+      collectedFee: [],
     };
     // Customizable Area End
     runEngine.attachBuildingBlock(this as IBlock, this.subScribedMessages);
@@ -75,6 +114,12 @@ export default class DashboardBudgetController extends BlockComponent<Props, S, 
         case this.GetAllBuildingListCallId:
           this.handleBuildingListResponse(responseJson);
           break;
+        case this.GetBudgetDashboardDataCallId:
+          this.handleBudgetDashboardDataResponse(responseJson);
+          break;
+        case this.GetBudgetDetailsCallId:
+          this.handleGetBudgetDetailsResponse(responseJson);
+          break;
         default:
           break;
       }
@@ -91,6 +136,76 @@ export default class DashboardBudgetController extends BlockComponent<Props, S, 
 
   // Customizable Area Start
   // Year List
+  getBudgetDashboardData = async () => {
+    const { filterYear, filterBuilding } = this.state;
+    const society_id = localStorage.getItem("society_id");
+
+    this.GetBudgetDashboardDataCallId = await apiCall({
+      contentType: "application/json",
+      method: "GET",
+      endPoint: `bx_block_dashboard/budget_dashbords?society_management_id=${society_id}&year=${filterYear}&building_management_id=${filterBuilding}`,
+    });
+  };
+
+  handleBudgetDashboardDataResponse = (responseJson: any) => {
+    if (responseJson && responseJson.data) {
+      const budgetDetails = responseJson.data;
+      this.setState({
+        budgetAmount: budgetDetails.attributes.budget_vs_collected.budget,
+        budgetCollected: budgetDetails.attributes.budget_vs_collected.collected,
+        rentDue: budgetDetails.attributes.total_rent_due_vs_collected.rent_due,
+        rentCollected: budgetDetails.attributes.total_rent_due_vs_collected.rent_collected,
+        member: budgetDetails.attributes.member_have_not_paid_management_fees.count,
+        totalExpense: budgetDetails.attributes.total_expenses.count,
+        rateSold: budgetDetails.attributes.occupancy_rate.sold,
+        rateUnsold: budgetDetails.attributes.occupancy_rate.unsold,
+        totalBudget: budgetDetails.attributes.remaining_budget_amount.count,
+        currency: budgetDetails.attributes.currency,
+        expenseBreakdown: Object.entries(budgetDetails.attributes.total_expenses_breakdown).map(([key, value]) => ({
+          key,
+          value,
+        })),
+        collectedFee: Object.entries(budgetDetails.attributes.collected_fees).map(([key, value]) => ({
+          key,
+          value,
+        })),
+      });
+    }
+  };
+
+  // Budget Details
+  getBudgetDetails = async () => {
+    const { filterYear, filterBuilding } = this.state;
+    const society_id = localStorage.getItem("society_id");
+
+    this.GetBudgetDetailsCallId = await apiCall({
+      contentType: "application/json",
+      method: "GET",
+      endPoint: `society_managements/${society_id}/bx_block_report/budget_reports/preview_budget?year=${filterYear}`,
+    });
+  };
+
+  handleGetBudgetDetailsResponse = (responseJson: any) => {
+    if (responseJson && responseJson.budget_report) {
+      this.setState({
+        approveAmount: responseJson.budget_report.approved_amount,
+        facility: responseJson.budget_report.facilities,
+        currency: responseJson.budget_report.currency.currency,
+      });
+    } else {
+      this.setState(
+        {
+          approveAmount: "",
+          facility: [],
+          currency: "",
+        },
+        () => {
+          toast.error(responseJson.message);
+        }
+      );
+    }
+  };
+
   getBudgetDashboardYearList = async () => {
     this.GetBudgetDashboardYearListCallId = await apiCall({
       contentType: "application/json",
@@ -107,10 +222,12 @@ export default class DashboardBudgetController extends BlockComponent<Props, S, 
 
   // Building List
   getAllBuildingList = async () => {
+    const society_id = localStorage.getItem("society_id");
+
     this.GetAllBuildingListCallId = await apiCall({
       contentType: "application/json",
       method: "GET",
-      endPoint: `bx_block_settings/building_managements`,
+      endPoint: `bx_block_settings/building_managements?society_management_id=${society_id}`,
     });
   };
 
